a9e7ab9144da8e1a4b916f62ae00a01c25165488
Otyr <otirmesi@gmail.com>
2025-01-11 16:41:31 +0200

    Assets

--- WorkFlows/bat_work_flow_1.bat
+++ WorkFlows/bat_work_flow_1.bat
@@ -9,10 +9,14 @@ Start git-bash
 Start explorer "C:\users\otirm_2hwnj4i\Desktop"
 Start explorer "C:\users\otirm_2hwnj4i\Documents\GitHub"
 
-REM opening mail + automation tools on power-on + sesonal choices(chrome acts as a more casual web browser)
+REM opening mail + Web-DashBoarded DevOps tools + sesonal choices(chrome acts as a more casual web browser)
 Start Chrome.exe https://mail.google.com/mail 
 Start Chrome.exe http://localhost:8080/
+Start Chrome.exe https://eu-north-1.console.aws.amazon.com/console/home?region=eu-north-1#
+Start Chrome.exe https://www.udemy.com/home/my-courses/learning/
 Start Chrome.exe https://drive.google.com/drive/folders/1CrfpU0yT7Znivsye9VCKP-DgwJw-2CCa
+REM the below might be needed manual change as the temporary url changes according to browser caching
+Start Chrome.exe file:///C:/Users/otirm_2hwnj4i/Downloads/Devops%20-%20%D7%9E%D7%91%D7%95%D7%90%20%D7%9C%204099108%20He%20%D7%90%D7%A9%D7%93%D7%95%D7%93%20%D7%AA%D7%A9%D7%A4%D7%93%20%D7%91.pdf
 
 REM opening version control browser dashboards
 Start Brave.exe https://github.com/otirmesilati
