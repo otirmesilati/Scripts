e7255da311d478c12c1f6d924117e51dfbe7bea2
Otyr <otirmesi@gmail.com>
2025-01-11 12:00:38 +0200

    Assets

--- WorkFlows/bat_work_flow_1.bat
+++ WorkFlows/bat_work_flow_1.bat
@@ -2,14 +2,25 @@
 
 @echo off
 
+REM opening shells, later - maybe theres a command for opening the frame with both lnx + psh together?
 Start wt.exe
+Start git-bash
+
 Start explorer "C:\users\otirm_2hwnj4i\Desktop"
 Start explorer "C:\users\otirm_2hwnj4i\Documents\GitHub"
-Start Chrome.exe
-Start Brave.exe
+
+REM opening mail + automation tools on power-on
+Start Chrome.exe https://mail.google.com/mail 
+Start Chrome.exe http://localhost:8080/
+
+REM opening version control browser dashboards
+Start Brave.exe https://github.com/otirmesilati
+Start Brave.exe https://codeberg.org/Otyr?tab=activity
+
+REM opening preferable dev tools
+Start notepad++.exe
 Start Code.exe
 Start GithubDesktop
 Start GitKraken
-
 Start kate
-Start git-bash
+
