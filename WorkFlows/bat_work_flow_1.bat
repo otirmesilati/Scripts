c4ca27aa3b113142142bb5f8366eab3bb6446d89
Otyr <45825931+otirmesilati@users.noreply.github.com>
2025-01-15 14:46:09 +0200

    Assets

--- WorkFlows/bat_work_flow_1.bat
+++ WorkFlows/bat_work_flow_1.bat
@@ -16,6 +16,7 @@ Start Chrome.exe http://localhost:8080/
 Start Chrome.exe https://eu-north-1.console.aws.amazon.com/console/home?region=eu-north-1#
 Start Chrome.exe https://www.udemy.com/home/my-courses/learning/
 Start Chrome.exe https://drive.google.com/drive/folders/1CrfpU0yT7Znivsye9VCKP-DgwJw-2CCa
+Start Chrome.exe https://eu2.make.com/organization/2739175/dashboard
 REM the below might be needed manual change as the temporary url changes according to browser caching
 Start Chrome.exe file:///C:/Users/otirm_2hwnj4i/Downloads/Devops%20-%20%D7%9E%D7%91%D7%95%D7%90%20%D7%9C%204099108%20He%20%D7%90%D7%A9%D7%93%D7%95%D7%93%20%D7%AA%D7%A9%D7%A4%D7%93%20%D7%91.pdf
 
