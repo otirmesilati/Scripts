8c19e628ec8ba4e3b9fc11e7928baa7d5ae1af40
Otyr <45825931+otirmesilati@users.noreply.github.com>
2025-01-25 08:58:05 +0200

    Assets

--- WorkFlows/bat_work_flow_1.bat
+++ WorkFlows/bat_work_flow_1.bat
@@ -18,7 +18,7 @@ Start Chrome.exe https://www.udemy.com/home/my-courses/learning/
 Start Chrome.exe https://drive.google.com/drive/folders/1CrfpU0yT7Znivsye9VCKP-DgwJw-2CCa
 Start Chrome.exe https://eu2.make.com/organization/2739175/dashboard
 REM the below might be needed manual change as the temporary url changes according to browser caching
-Start Chrome.exe file:///C:/Users/otirm_2hwnj4i/Downloads/Devops%20-%20%D7%9E%D7%91%D7%95%D7%90%20%D7%9C%204099108%20He%20%D7%90%D7%A9%D7%93%D7%95%D7%93%20%D7%AA%D7%A9%D7%A4%D7%93%20%D7%91.pdf
+Start Chrome.exe file:///C:/Users/otirm_2hwnj4i/Downloads/lab5_func.pdf
 
 REM opening version control browser dashboards
 Start Brave.exe https://github.com/otirmesilati
